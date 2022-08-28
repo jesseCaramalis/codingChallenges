fc64f2dfdc8c004f205d48a1db6ce628c635047d
Jesse Caramalis <jessecaramalis95@gmail.com>
2022-08-28 16:39:01 +1000

    coding challenge 28/08/2022

---
+++ 8correctCharacterSoftware.js
@@ -0,0 +1,25 @@
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+//     S is misinterpreted as 5
+//     O is misinterpreted as 0
+//     I is misinterpreted as 1
+
+// The test cases contain numbers only by mistake.
+
+//Answer
+
+
+
+function correct(string){
+  
+    return string.split('').map(x=>{
+      if(x == '0'){return 'O'}
+      if(x == '5'){return 'S'}
+      if(x == '1'){return 'I'}
+      else{return x}
+      }).join('')
+    }
