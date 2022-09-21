f95069cb2dba03cef8a95fec5b5176221495cf2b
jesseCaramalis <jessecaramalis95@gmail.com>
2022-09-21 12:18:49 +1000

    coding challenge 21/09/2022

---
+++ 8isDivisible.js
@@ -0,0 +1,5 @@
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+
+// Answer
+
+const isDivisible = (n, x, y) => n%x === 0 && n%y === 0
