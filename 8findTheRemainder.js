e73ec3b353aae0299ab2324ab60bde1a2e23e4d4
jesseCaramalis <jessecaramalis95@gmail.com>
2022-11-24 12:30:56 +1000

    coding challenge 24/11/2022

---
+++ 8findTheRemainder.js
@@ -0,0 +1,5 @@
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Answer
+
+const remainder = (n, m) => n > m ? n%m : m%n
