d801f61ed588bd0f1072bbfec9bd5b0af39d8547
jesseCaramalis <jessecaramalis95@gmail.com>
2022-11-27 11:02:54 +1000

    coding challenge 27/11/2022

---
+++ 8findThePosition.js
@@ -0,0 +1,12 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+// Answer
+
+function position(letter){ 
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    return `Position of alphabet: ${alphabet.indexOf(letter)+1}`
+  }
