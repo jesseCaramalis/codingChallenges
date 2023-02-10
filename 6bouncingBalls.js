4c08b3b2c1ab2121622d8bc27f9952373ac7cdb7
Jesse Caramalis <101160675+jesseCaramalis@users.noreply.github.com>
2023-02-10 13:39:42 +1000

    coding challenge 10/02/2022

---
+++ 6bouncingBalls.js
@@ -0,0 +1,49 @@
+// A child is playing with a ball on the nth floor of a tall building. The height of this floor above ground level, h, is known.
+
+// He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+// His mother looks out of a window 1.5 meters from the ground.
+
+// How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+// Three conditions must be met for a valid experiment:
+// Float parameter "h" in meters must be greater than 0
+// Float parameter "bounce" must be greater than 0 and less than 1
+// Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+// Note:
+// The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+
+// Examples:
+// - h = 3, bounce = 0.66, window = 1.5, result is 3
+
+// - h = 3, bounce = 1, window = 1.5, result is -1 
+
+// (Condition 2) not fulfilled).
+
+// Answer
+
+//First check for three conditions: h > 0, 0 < bounce < 1, window < h
+//if true, establish count var as 0. Create for loop where i = h, i > window, no increment.
+//We will write loop to model situation: Ball is dropped from h, it will pass the window there so count + 1.
+//Then the ball bounces, so i = i * bounce. If i is sitll greater than window,  count + 1.
+//Loop ends and checks if i is still greater than h. Bounce check occurs within loop.
+//return count, else return -1 at end of function.
+
+function bouncingBall(h,  bounce,  window) {
+    if(h>0 && 1>bounce && bounce>0 && window<h){
+      let ballCount = 0
+      for(let i = h; i > window; i){
+        ballCount += 1
+        i*= bounce
+        if(i>window){
+          ballCount +=1
+        }
+      }
+      return ballCount
+    }
+    else{
+      return -1
+    }
+  }
